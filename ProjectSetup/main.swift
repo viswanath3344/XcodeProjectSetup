a670b1e9351cdfd7930db2e949836809606c3677
Viswanath Reddy Ponthota (Contractor) <viswanathreddy.ponthota@cognizant.com>
2024-08-06 17:32:19 +0530

    Disconnect your app from unit-testing
    https://samwize.com/2023/01/18/disconnect-your-app-from-unit-testing/

--- ProjectSetup/main.swift
+++ ProjectSetup/main.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// MARK: Ref: https://samwize.com/2023/01/18/disconnect-your-app-from-unit-testing/
 private func delegateClassName() -> String? {
     if NSClassFromString("XCTestCase") != nil { // Unit Testing
         return nil
