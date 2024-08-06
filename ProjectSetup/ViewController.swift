f77435e51e5f32ab15baaae05dd709e88290414b
Viswanath Reddy Ponthota (Contractor) <viswanathreddy.ponthota@cognizant.com>
2024-08-06 11:38:31 +0530

    Initial Commit

---
+++ ProjectSetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectSetup
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 06/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
