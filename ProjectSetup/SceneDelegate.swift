2628bda967e42c8a85bac1ad88b0856ec2009c4f
Viswanath Reddy Ponthota (Contractor) <viswanathreddy.ponthota@cognizant.com>
2024-08-06 20:44:33 +0530

    Created Swiftgen configuration and   generated the assets file for Color and Images. Modify swiftlint to disable the name.

--- ProjectSetup/SceneDelegate.swift
+++ ProjectSetup/SceneDelegate.swift
@@ -15,7 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIHostingController(rootView: Text("Hello"))
+        window?.rootViewController = UIHostingController(rootView: Text("Hello").background(content: {
+            Asset.appColor.swiftUIColor
+        }))
         window?.makeKeyAndVisible()
     }
 
--- ProjectSetup/main.swift
+++ ProjectSetup/main.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 // MARK: Ref: https://samwize.com/2023/01/18/disconnect-your-app-from-unit-testing/
+
 private func delegateClassName() -> String? {
     if NSClassFromString("XCTestCase") != nil { // Unit Testing
         return nil
