d649225df24bc0fa8d2722bd5f71ed14da804856
Viswanath Reddy Ponthota (Contractor) <viswanathreddy.ponthota@cognizant.com>
2024-08-06 13:27:00 +0530

    Added the SwiftFormat for formatting the iOS project.  Installed Swift Format in Mac machine with help of Brew and configured the scripts in build phases -> build scripts.
    The other changes are added from swift format when it job done.

--- ProjectSetupTests/ProjectSetupTests.swift
+++ ProjectSetupTests/ProjectSetupTests.swift
@@ -5,11 +5,10 @@
 //  Created by Ponthota, Viswanath Reddy (Cognizant) on 06/08/24.
 //
 
-import XCTest
 @testable import ProjectSetup
+import XCTest
 
 final class ProjectSetupTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class ProjectSetupTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- ProjectSetupUITests/ProjectSetupUITestsLaunchTests.swift
+++ ProjectSetupUITests/ProjectSetupUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class ProjectSetupUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
